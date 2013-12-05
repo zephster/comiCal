1d816be52122b9d6c7b70000de30c878b09e3969
brandon <antizeph@gmail.com>
2013-12-05 10:48:36 -0500

    initial commit

---
+++ importer.py
@@ -0,0 +1,155 @@
+"""
+comic importer - scrapes comic sites then imports release dates into google calendar
+by brandon sachs
+ 
+relevant docs i may want to read:
+    http://docs.python.org/2/library/queue.html
+    http://docs.python.org/2/library/pickle.html
+        https://wiki.python.org/moin/UsingPickle
+     
+css info paths
+    dc - .view-comics .row-1 .col-1/2/3/4 (.row-1 contains last 4 issues, in their columns, newest to oldest)
+    image - .inyards .main_area .left .latest_releases > .release_box (nth-child(1) for the latest, 2 for last month)
+ 
+    for marvel
+        iterate over the selector
+            4 times - get latest 4? issues link urls. those selectors are just divs, newest to oldest
+            go to each of those urls
+                publish date selector: .featured-item-meta - need to parse out date
+     
+notes/ideas/whatever
+    use cPickle to save/load list of comics
+        can add sys opts to add/remove comics on-the-fly
+            -a --add
+                -p --publisher  string (dc|marvel|imagine else throw)
+                -t --title      string (name of the comic)
+                -u --uri        string
+            -r --remove
+                -p --publisher string (dc|marvel|imagine else throw)
+                -t --title     string (when checking if it exists, normalize in lowercase or something, or use a case-insensitive flag if there is one)
+ 
+    have an option to format comic title in uppercase or camel case?
+        default to camel case?
+        -f --format string (uppercase|camelcase else throw)
+ 
+    when iterating through release_dates for gcal, if possible, search for the comic title first to see if it's already in the cal, then check the date of it. if the date is wrong, re-schedule to newer scraped date
+"""
+ 
+ 
+import requests
+from bs4 import BeautifulSoup
+ 
+ 
+base_url = {
+    "dc"    : "http://www.dccomics.com/comics/",
+    "image" : "http://www.imagecomics.com/comics/series/",
+    "marvel": "http://marvel.com/comics/series/"
+}
+ 
+comics = {
+    "dc" : {
+        "Justice League"              : "justice-league-2011",
+        "Superman"                    : "superman-2011",
+        "Superman Unchained"          : "superman-unchained-2013",
+        "Green Lantern"               : "green-lantern-2011",
+        "Green Lantern Corps"         : "green-lantern-corps-2011",
+        "Green Lantern New Guardians" : "green-lantern-new-guardians-2011"
+    },
+    "marvel" : {
+        "Superior Spider-Man" : "17554/superior_spider-man_2013_-_present",
+        "Ultimate Spider-Man" : "13831/ultimate_comics_spider-man_2011_-_present"
+    },
+    "image" : {
+        "The Walking Dead" : "the-walking-dead"
+    }
+}
+ 
+ 
+ 
+release_dates = {
+    "dc"    : {},
+    "marvel": {},
+    "image" : {}
+}
+ 
+selectors = {
+    "dc" : ".row-1 td",
+    "marvel" : ".JCMultiRow-comic_issue > .comic-item" # untested, see notes
+}
+ 
+ 
+ 
+def main():
+    for title, url in comics["dc"].iteritems():
+        #print title, url
+        pass
+     
+    #manual greenlantern test for now
+    scrape_dc("Green Lantern", comics["dc"]["Green Lantern"])
+    #scrape_dc("Green Lantern Corps", comics["dc"]["Green Lantern Corps"])
+    print release_dates
+     
+    # for title, url in comics["dc"].iteritems():
+    #     scrape_dc(title, url)
+     
+    #to add to release dates dict
+    #release_dates["publisher here"]["series here"] = "release date here"
+    #print release_dates
+     
+     
+# scrape imagecomics.com
+def scrape_image(comic_title, uri):
+    pass
+ 
+ 
+# scrape marvel.com
+def scrape_marvel(comic_title, uri):
+    pass
+ 
+     
+# scrape dccomics.com
+def scrape_dc(comic_title, uri):
+    url = base_url["dc"] + uri
+    print "scraping %s" % url
+     
+    try:
+        r = requests.get(url)
+ 
+        # count = 0
+        # for title in soup.select(".row-1 .views-field-title > .field-content a"):
+        #     print title.text
+        #     # limit to only the 2 newest issues
+        #     # count += 1
+        #     # if count >= 2:
+        #     #     break
+         
+        try:
+            soup = BeautifulSoup(r.text.encode("utf-8"))
+             
+            for issue in soup.select(selectors["dc"]):
+                title = issue.text[:-22].strip() # removes the "on sale" date
+                date  = issue.text[-13:].strip() # removes comic title
+                release_dates["dc"][title] = date
+                #print "%s is being released on %s" % (title, date)
+             
+        except Exception as e:
+            print "error parsing %s" % url
+            print e
+         
+    except Exception as e:
+        print "error fetching %s" % url
+        print e
+ 
+ 
+ 
+ 
+     
+     
+     
+     
+     
+     
+ 
+ 
+if __name__ == '__main__':
+  main()
